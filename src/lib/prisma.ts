a4aee0227e56f497c7fcd33ed209be4fe35c9bdf
DoctorCel <doctorcelmed@gmail.com>
2024-09-09 14:25:45 -0500

    prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,12 @@
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
 
-export default prisma;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
