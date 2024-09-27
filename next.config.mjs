dcad01f9e57f875f7d4cb22fc4d4e10f11e5de62
DoctorCel <doctorcelmed@gmail.com>
2024-09-26 20:21:09 -0500

    https

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,62 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'self';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: res.cloudinary.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=self, microphone=(), geolocation=()'
+  }
+]
+
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-      images: {
-        domains: ['res.cloudinary.com'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
       },
-    reactStrictMode: true,
-    experimental: {
-      serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
-    },
+    ]
+  },
 };
 
 export default nextConfig;
